0a7ceaa61ef9da95a09fcaae4841a5a040266228
Oscar <oscarmappley@icloud.com>
2023-06-16 01:20:07 +0100

    added highest lowest game

--- script.js
+++ script.js
@@ -47,3 +47,83 @@ function buyIncreaseIncrease()
     }
     showChanges();
 }
+
+let websites = {
+    "google.com": 106500000000,
+    "youtube.com": 94500000000,
+    'microsoft.com': 1400000000,
+    'linkedin.com': 1400000000,
+    'apple.com': 665000000,
+    'wikipedia.org': 7600000000,
+    'cloudflare.com': 105300000,
+    'whatsapp.com': 160000000
+}
+
+let idToWebsite = {
+    'website1': '',
+    'website2': ''
+}
+
+let score = 0;
+
+function game()
+{
+    let indexA = Math.floor(Math.random() * Object.keys(websites).length);
+    let indexB = Math.floor(Math.random() * Object.keys(websites).length);
+    let siteA = Object.keys(websites)[indexA];
+    let siteB = Object.keys(websites)[indexB];
+    document.getElementById('website1').innerHTML = siteA;
+    document.getElementById('website2').innerHTML = siteB;
+    idToWebsite['website1'] = siteA;
+    idToWebsite['website2'] = siteB;
+    document.getElementById('visitors shower').innerHTML = '? | ?'
+}
+
+function pickSite(picked, unpicked)
+{
+    pickedSite = idToWebsite[picked];
+    unpickedSite = idToWebsite[unpicked];
+
+    pickedSiteVisitors = websites[pickedSite];
+    unpickedSiteVisitors = websites[unpickedSite];
+
+    if (pickedSiteVisitors >= unpickedSiteVisitors)
+    {
+        showVisitors(websites[idToWebsite['website1']], websites[idToWebsite['website2']]);
+        score++;
+        document.getElementById('score').innerHTML = 'score: ' + score;
+    }
+    else
+    {
+        score = 0;
+        document.getElementById('score').innerHTML = 'score: ' + score;
+        game();
+    }
+}
+
+function showVisitors(visitors1, visitors2)
+{
+    document.getElementById('visitors shower').innerHTML = visitors1 + ' | ' + visitors2;
+}
+
+function nextRound()
+{
+    let randomChoice = Math.floor(Math.random() * 2) + 1;
+
+    console.log(randomChoice)
+
+    if (randomChoice == 1)
+    {
+        idToWebsite['website1'] = Object.keys(websites)[Math.floor(Math.random() * Object.keys(websites).length)];
+        document.getElementById('website1').innerHTML = idToWebsite['website1'];
+        let visitors2 = websites[idToWebsite['website2']];
+        document.getElementById('visitors shower').innerHTML =  '? | ' + visitors2;
+    }
+    else
+    {
+        idToWebsite['website2'] = Object.keys(websites)[Math.floor(Math.random() * Object.keys(websites).length)];
+        document.getElementById('website2').innerHTML = idToWebsite['website2'];
+        let visitors1 = websites[idToWebsite['website1']];
+        document.getElementById('visitors shower').innerHTML =  visitors1 + ' | ?';
+    }
+}
